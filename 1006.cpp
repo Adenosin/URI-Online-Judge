f1d2bd3a081f70821b231aa908fb1b4e5b6350a7
Rodrigo Pari Susaño <rodrigo.parisusao25201@gmail.com>
2020-08-11 07:12:43 -0400

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float A, B, C, media;
+    cin >> A >> B >> C;
+    media = (A*2+B*3+C*5)/10;
+    cout << "MEDIA = " << fixed << setprecision(1) << media << endl;
+    return 0;
+}
