23034e00a2316a36187344fb84260bda040c3564
Rodrigo Pari Susaño <rodrigo.parisusao25201@gmail.com>
2020-08-11 20:54:33 -0400

    Create 1097.cpp

---
+++ 1097.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1, j=7;
+    while (i<11)
+    {
+        cout << "I=" << i << " J=" << j << endl;
+        j--;
+        cout << "I=" << i << " J=" << j << endl;
+        j--;
+        cout << "I=" << i << " J=" << j << endl;
+        j+=4;
+        i+=2;
+    }
+    return 0;
+}
