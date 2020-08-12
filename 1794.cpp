4b8298c57b85f82b321ab500a61c23d23d100708
Rodrigo Pari Susaño <rodrigo.parisusao25201@gmail.com>
2020-08-11 21:23:57 -0400

    Create 1794.cpp

---
+++ 1794.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, la, lb, sa, sb;
+    cin >> n;
+    cin >> la >> lb;
+    cin >> sa >> sb;
+    if (n>=la and n>=sa and n<= lb and n<=sb)
+        cout << "possivel" << endl;
+    else
+        cout << "impossivel" << endl;
+    return 0;
+}
